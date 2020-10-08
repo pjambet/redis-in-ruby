8e9597d4125360f20118ba6f3461dff6e6f71ef5
Pierre Jambet <pierre.jambet@gmail.com>
2020-10-08 15:01:12 -0400

    Skeleton for tests

---
+++ code/chapter-8/test/hash_test.rb
@@ -0,0 +1,57 @@
+# coding: utf-8
+
+require_relative './test_helper'
+
+describe 'BYORedis - Hash commands' do
+  describe 'HSET' do
+    it 'rejects an invalid number of arguments'
+  end
+
+  describe 'HGETALL' do
+    it 'rejects an invalid number of arguments'
+  end
+
+  describe 'HGET' do
+    it 'rejects an invalid number of arguments'
+  end
+
+  describe 'HDEL' do
+    it 'rejects an invalid number of arguments'
+  end
+
+  describe 'HEXISTS' do
+    it 'rejects an invalid number of arguments'
+  end
+
+  describe 'HINCRBY' do
+    it 'rejects an invalid number of arguments'
+  end
+
+  describe 'HINCRBYFLOAT' do
+    it 'rejects an invalid number of arguments'
+  end
+
+  describe 'KEYS' do
+    it 'rejects an invalid number of arguments'
+  end
+
+  describe 'HLEN' do
+    it 'rejects an invalid number of arguments'
+  end
+
+  describe 'HMGET' do
+    it 'rejects an invalid number of arguments'
+  end
+
+  describe 'HSETNX' do
+    it 'rejects an invalid number of arguments'
+  end
+
+  describe 'HSTRLEN' do
+    it 'rejects an invalid number of arguments'
+  end
+
+  describe 'HVAL' do
+    it 'rejects an invalid number of arguments'
+  end
+end
